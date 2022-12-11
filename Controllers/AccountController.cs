b9dd08c5c25f625a737cb3ef30c3af619433f7e7
dudgang <98886758+dudgang@users.noreply.github.com>
2022-12-11 18:09:08 +0300

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyWebsiteFramwork.Models;
+using System.Data.SqlClient;
+
+namespace MyWebsiteFramwork.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source = DESKTOP-51T57VE; database = Account; integrated security = SSPI; ";
+        }
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "Select * from  Login where username ='"+acc.Name+"' and password ='"+acc.Password+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Dashboard"); 
+            }
+            else
+            {
+                con.Close();
+                return View("Register");
+            }
+        }
+
+        [HttpGet]
+        // GET: Createapp
+        public ActionResult App()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        // GET: Createapp
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        // GET: Createapp
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyWebsiteFramwork.Models
+{
+    public class App
+    {
+        [Key]
+        public int OppId { get; set; }
+        public string Name { get; set; }
+        public  int CreatedbyUserId { get; set; }
+    }
+}
